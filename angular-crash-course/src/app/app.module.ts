ae4bfb286974a8e4e8bcbab410b98c6f1ef6a1cd
a.sinitsyn <sinitsyn88@gmail.com>
2024-09-28 22:50:44 +0100

    Declarative and programmatic way to navigate to the home and about tabs

--- angular-crash-course/src/app/app.module.ts
+++ angular-crash-course/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import { FormsModule } from "@angular/forms";
 import { MessageDetailsComponent } from "./message-details/message-details.component";
 import {MyFirstService} from "./services/my-first.service";
+import {MenuComponent} from "./menu/menu.component";
+import {AboutComponent} from "./about/about.component";
 
 @NgModule({
   // Need to declare new modules here
   declarations: [
     AppComponent,
     MyFirstCompComponent,
-    MessageDetailsComponent
+    MessageDetailsComponent,
+    MenuComponent,
+    AboutComponent
   ],
   // Import modules that we need for our application
   imports: [
