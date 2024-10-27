acf8b22db61e8c71064f32a3e3ec2856ea2d594c
a.sinitsyn <sinitsyn88@gmail.com>
2024-10-27 23:28:46 +0000

    Added Login page functionality

--- book-social-network/book-network-ui/src/app/app.module.ts
+++ book-social-network/book-network-ui/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideHttpClient, withFetch } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from "./pages/login/login.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent]
